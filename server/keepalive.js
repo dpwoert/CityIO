c40b23408bd61007e4072e4e16a75c29f1ed5e71
davey <_davey_@live.nl>
2014-02-15 23:19:47 +0100

    bugfix

--- server/keepalive.js
+++ server/keepalive.js
@@ -1,6 +1,6 @@
 function keepAlive() {
     setInterval(function() {
-        HTTP.get('http://city3d.herokuapp.com');
+        Meteor.http.get('http://city3d.herokuapp.com');
         keepAlive();
     }, 20 * 58 * 1000);
 }
