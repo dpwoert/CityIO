22b7165490129ebd5345c8ef9570c552900367ad
davey <_davey_@live.nl>
2014-02-16 20:14:12 +0100

    fiber bugfix

--- server/keepalive.js
+++ server/keepalive.js
@@ -1,8 +1,13 @@
-function keepAlive() {
-    setInterval(function() {
-        Meteor.http.get('http://city3d.herokuapp.com');
-        keepAlive();
-    }, 20 * 58 * 1000);
-}
+var Fiber = Npm.require('fibers');
+Fiber(function(){
 
-keepAlive();
+	function keepAlive() {
+	    setInterval(function() {
+	        Meteor.http.get('http://city3d.herokuapp.com');
+	        keepAlive();
+	    }, 20 * 58 * 1000);
+	}
+
+	keepAlive();
+    	
+}).run();
