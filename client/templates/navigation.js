0f478a2d96848afa58488d94c46ce5cdb5393449
davey <_davey_@live.nl>
2014-02-23 19:47:52 +0100

    removed zipcodes

--- client/templates/navigation.js
+++ client/templates/navigation.js
@@ -1,17 +1,7 @@
 //navigation
 Template.navigation.rendered = function(){
 
-	//postcode
-	var zipInput = this.find('input.code');
-	var zipButton = this.find('input.go');
-
-	zipButton.onclick = function(){
-
-		var show = new PostalCode(zipInput);
-
-	}
-
-	//day/night
+	//day/night elements
 	var day = this.find('span.day');
 	var night = this.find('span.night');
 
