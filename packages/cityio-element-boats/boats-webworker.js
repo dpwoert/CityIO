3f5037aae84cac86aefac3f454aba6f2939f6fc3
davey <_davey_@live.nl>
2014-04-12 00:48:52 +0200

    load boats

--- packages/cityio-element-boats/boats-webworker.js
+++ packages/cityio-element-boats/boats-webworker.js
@@ -3,17 +3,45 @@
 //callback
 self.addEventListener('message', function(e) {
 
-    if(e.data == 'start'){
-        //started
-    }
+    //todo something here?
 
 }, false);
 
-//update loop [10sec]
-var update = function(){
+var testBoat = {
+    id: 1,
+    name: 'Pakjesboot 1',
+    model: 'ship',
+    position: [ 4.475384, 51.901984 ],
+    rotation: 120,
+};
 
-     postMessage('worker update');
-     setTimeout(update, 1000*10);
+var i = 0;
+
+var list = {
+
+    add: [ testBoat ],
+    remove: [],
+    change: []
+
+};
+
+//update loop [10sec]
+var update = function(speed){
+
+     postMessage(list);
+
+    //reset
+    i++;
+    list.change = [];
+    list.remove = [];
+    list.add = [];
+
+    if(i == 10){
+        testBoat.position = [4.49167, 51.915003];
+        list.change.push(testBoat);
+    }
+
+    setTimeout(update, 1000*10);
 }
 
 update();
--- packages/cityio-element-boats/boats.js
+++ packages/cityio-element-boats/boats.js
@@ -5,11 +5,18 @@ if(!IO || !IO.elements){
 
 IO.elements.Boats = function(scene, settings){
 
+	//ship objects
 	var boatList = [];
 	var geoms = {};
 	var models = {
 		ship: '/3dmodels/ship2.json'
 	};
+
+	//3d material
+	var material = new THREE.MeshLambertMaterial({
+		color: 0x333333,
+		shading: THREE.FlatShading
+	})
 
 	this.load = function(){
 
@@ -55,16 +62,118 @@ IO.elements.Boats = function(scene, settings){
 		this.live(e.data);
 	}.bind(this);
 
+	var getBoat = function(id, key){
+
+		for( var i = 0 ; i < boatList.length ; i++ ){
+			//found
+			if (boatList[i].id == id){
+				if(key) return i;
+				return boatList[i];
+			}
+		}
+
+		//not found
+		return false;
+	}
+
+	var addBoat = function(options){
+
+		var geom = geoms[options.model];
+		var mesh = new THREE.Mesh(geom, material);
+
+		//rotate & scale
+		IO.group.add(mesh);
+		mesh.scale.set(4,4,4);
+		mesh.rotateX(Math.PI/2);
+		mesh.rotateY(THREE.Math.degToRad(options.rotation));
+
+		//set initial position
+		var points = IO.points.translate2D( options.position );
+		var points3d = new THREE.Vector3(points.x,points.y,-3);
+		mesh.position = points3d;
+
+		boatList.push({
+			'id': options.id,
+			'mesh': mesh,
+			'from': points3d,
+			'to': points3d,
+			'rotFrom': options.rotation,
+			'rotTo': options.rotation
+		});
+
+	};
+
+	var deleteBoat = function(id){
+		var boatID = getBoat(id, true);
+		var boat = boatList[boatID];
+
+		//remove
+		IO.group.remove(boat);
+		boatList.slice(boatID,1);
+	};
+
+	var changeBoat = function(options){
+		var _to = IO.points.translate2D( options.position );
+		var to = new THREE.Vector3(_to.x,_to.y,-3);
+		var boat = getBoat(options.id);
+		var from = boat.to;
+		boat.to = to;
+		boat.from = from;
+	};
+
+	var lerp3d = function(from, to, time){
+		return new THREE.Vector3(
+			from.x + time * (to.x - from.x),
+			from.y + time * (to.y - from.y),
+			from.z + time * (to.z - from.z)
+		);
+	}
+
 	this.render = function(){
 
-	};
+		var current = +new Date();
+		var progress = (this.futureUpdate - this.lastUpdate)/(current - this.lastUpdate);
+		console.log(progress);
+		for( var i = 0 ; i < boatList.length ; i++ ){
+
+			var boat = boatList[i];
+			boat.mesh.position = lerp3d(boat.from, boat.to, progress);
+			//add rotation
+
+		}
+
+	}.bind(this);
 
 	this.live = function(api){
-		//api change
-		//api remove
-		//api add
+
+		//save update time
+		var prevUpdate = this.lastUpdate || +new Date();
+		this.lastUpdate = +new Date();
+		this.futureUpdate = this.lastUpdate + (this.lastUpdate - prevUpdate);
+
+		//render
+		if(!this.rendering){
+			IO.renderList.push(this.render);
+			this.rendering = true;
+		}
 
 		console.log(api);
-	};
+
+		//add boats
+		for( var i = 0; i < api.add.length ; i++ ){
+			addBoat(api.add[i]);
+		}
+
+		//delete boats
+		for( var i = 0; i < api.remove.length ; i++){
+			removeBoat(api.remove[i]);
+		}
+
+		//change boats
+		for( var i = 0; i < api.remove.length ; i++){
+			changeBoat(api.remove[i]);
+		}
+
+	}.bind(this);
 
 };
