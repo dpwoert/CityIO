1114d1c6a00f953cd43bf75e2bb8f6570f7cdd0c
davey <davey@dpwoert.com>
2015-01-14 12:02:14 +0100

    boilerplate

---
+++ client/classes/timeline.js
@@ -0,0 +1,27 @@
+module.exports = function(){
+
+    var update = function(){
+
+    };
+
+    this.addPoint = function(fn, time){
+
+    };
+
+    this.addEvent = function(fn, from, to){
+
+    };
+
+    this.start = function(){
+
+    };
+
+    this.pause = function(){
+
+    };
+
+    this.reset = function(){
+
+    };
+
+}
