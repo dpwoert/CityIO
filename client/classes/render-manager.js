af18c2fccc4c2043ade0706436985e04da1db991
davey <davey@dpwoert.com>
2015-04-26 20:17:09 +0200

    named process for more control

--- client/classes/render-manager.js
+++ client/classes/render-manager.js
@@ -6,16 +6,59 @@ module.exports = function(){
 	var end = false;
 	var clock = new THREE.Clock();
 
-	this.add = function(fn){
+	var createProcess = function(a1, a2){
 
-		var id = THREE.Math.generateUUID();
+		var id, fn;
 
-		list.push({
+		if(a1 instanceof Function){
+			id = THREE.Math.generateUUID();
+			fn = a1;
+		} else{
+			id = a1;
+			fn = a2;
+
+			//prevent double id's
+			this.remove(id);
+		}
+
+		return {
 			'task': fn,
 			'id': id
-		});
+		};
 
-		return id;
+	}.bind(this);
+
+	this.add = function(a1, a2){
+
+		var _process = createProcess(a1, a2);
+		list.push(_process);
+		return _process.id;
+
+	};
+
+	this.addBefore = function(before, a1, a2){
+
+		var _process = createProcess(a1, a2);
+
+		//add to correct place
+		var index = list.indexOf(before);
+		list.splice(index, 0, _process);
+
+		return _process.id;
+
+	};
+
+	this.addAfter = function(a1, a2, before){
+
+		before = before || a2;
+		var _process = createProcess(a1, a2);
+
+		//add to correct place
+		var index = list.indexOf(before) + 1;
+		list.splice(index, 0, _process);
+
+		return _process.id;
+
 	};
 
 	this.remove = function(id){
